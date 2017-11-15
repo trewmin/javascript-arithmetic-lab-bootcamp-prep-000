c39c80a54414c56a5183a180eb0c1b0c5bc38b0a
Truman Lahr <trumanlahr@gmail.com>
2017-11-15 14:03:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,17 @@ function subtract(a,b) {
 }
 
 function multiply(a,b) {
-  return (a*b)
+  return(a*b)
 }
 
 function divide(a,b) {
-  return (a/b)
+  return(a/b)
+}
+
+function inc(n) {
+  return(n+1)
+}
+
+function dec(n) {
+  return(n-1)
 }
